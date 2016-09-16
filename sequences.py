33fb635017121fa882ef724a1525c898d4298030
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-09-16 20:23:56 +0000

    minor changes

--- sequences.py
+++ sequences.py
@@ -9,36 +9,44 @@ Please do not input a negative number.
 
 """
 
+
 def fibonacci(n):
     """Function Docstring
     fibonacci(int) -> list 
     
     this function will take in an integer n and finds the n sequences of fibonacci
-    and will return the list of the n sequences.
+    and will return the list of the n sequences. This function has a catch to see if the user
+    input is a negative number, in which an error message will display, the number converted
+    to its non-negative equivalent, and then the function runs with the new positive input.
     """
     if n < 0:
         print ("The number is negative. Will change to a positive number")
         n = -n
         
-    #setting our initial conditions
+    #setting our initial condition variables, list, and counter variable at 3
     a1, a2 = 1, 1
     final_list = [1]
     counter = 3
 
+    #first checks if the user input was numbers 1 or 2 as those are part of our initial conditons 
+    #then returns the list appropriately
     if n == 1:
 	    return final_list
     elif n == 2:
         final_list.append(1)
         return final_list
+    #if the inputed number is greater than 2, we initiate our code to calculate the fibonacci sequences
     else:
+    # append 1 to the list, 
+    # now our lists starts with two 1s, per our initial conditions
         final_list.append(1)
+        #while our counter is less than or equal to the user input, we add the previous two numbers
+        #of the list, append it to our list, then reset our variables
         while counter <= n:
-            new_num = a1 + a2
-            final_list.append(new_num)
+            a1, a2 = a2, a1 + a2 
+            final_list.append(a2)
             
-            # resets the previous number variables
-            a1 = a2
-            a2 = new_num
+            # push up the counter
             counter = counter + 1
     
     return final_list
