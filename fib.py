88505e795b7b6c5e4641eadf80bb63be92caa331
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-09-16 20:24:47 +0000

    added check for negative number input

--- fib.py
+++ fib.py
@@ -1,8 +1,17 @@
 #takes all files from sequences.py
 import sequences
 
+"""Module Description
+This function uses the fibonacci function from sequences.py
+to find the n squences of the fibonacci series. 
+The function then returns the last number of the sequences and
+outputs it. 
+This script only needs to be called as a module and does not run any code.
+
+"""
+
 def main(n):
-    x=sequence.fibonacci(n)
+    x=sequences.fibonacci(n)
     #prints the last element of the fibonacci sequence 
     print x[-1]
 
@@ -14,5 +23,10 @@ if __name__ == "__main__":
         print("Error:did not input a number. Will set a default argument of 5")
         main(5)
     else:
-        main(int(argv[1]))
+        # checks to see if 
+        n = int(argv[1])
+        if n < 0:
+            print("Error: Negative number. Will convert to positive counterpart")
+            n = -n
+        main(n)
 
