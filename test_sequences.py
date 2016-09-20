087afb73afd679c413368cb6c44b68020031220f
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-09-20 23:05:01 +0000

    removed print statements

--- test_sequences.py
+++ test_sequences.py
@@ -6,14 +6,11 @@ def test_fibonacci():
     fib_list=sequences.fibonacci(5)
     test_list=[1,1,2,3,5]
     assert fib_list == test_list
-    print "test 1 true"
-    
+
     fib_list=sequences.fibonacci(1)
     test_list=[1]
     assert fib_list == test_list
-    print "test 2 true"
 
     fib_list=sequences.fibonacci(10)
     test_list=[1,1,2,3,5,8,13,21,34,55]
     assert fib_list == test_list
-    print "test 3 true"
