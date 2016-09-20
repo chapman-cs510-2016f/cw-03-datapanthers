d1435def705f647bf5f1d985e44bc84405583120
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-09-20 23:05:26 +0000

    added more test functions

--- testfib.py
+++ testfib.py
@@ -3,6 +3,22 @@
 import fib
 
 def test_fib():
+    # test when input is 5
     test=5
     result=fib.main(5)
+    assert test==result, "result = " + str(result)
+    
+    # test when input 1
+    test=1
+    result=fib.main(1)
+    assert test==result, "result = " + str(result)
+    
+    # test when input is 2
+    test=1
+    result=fib.main(1)
+    assert test==result, "result = " + str(result)
+    
+    # test when input is a negative number
+    test=5
+    result=fib.main(-5)
     assert test==result, "result = " + str(result)
