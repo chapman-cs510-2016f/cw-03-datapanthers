1a77231dcbb96eef48c8567300bde0c9d6fd1f60
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-09-20 23:09:48 +0000

    added more test functions

--- test_sequences.py
+++ test_sequences.py
@@ -2,6 +2,7 @@
 
 import sequences
 
+# this function imports the sequences.py and tests the fibonacci function to check if it returns the expected list. 
 def test_fibonacci():
     fib_list=sequences.fibonacci(5)
     test_list=[1,1,2,3,5]
@@ -13,4 +14,9 @@ def test_fibonacci():
 
     fib_list=sequences.fibonacci(10)
     test_list=[1,1,2,3,5,8,13,21,34,55]
+    assert fib_list == test_list
+
+    # test to make sure negative input works
+    fib_list=sequences.fibonacci(-5)
+    test_list=[1,1,2,3,5]
     assert fib_list == test_list
