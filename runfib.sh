d1b95be1684282123164846e5a76a9f1693bf496
Justin Dressel <prof.justin.dressel@gmail.com>
2016-09-27 17:13:49 -0700

    instructor comments

--- runfib.sh
+++ runfib.sh
@@ -15,11 +15,22 @@ array=()
 # For loop implementing our fib.py for each number in 1 to 10000
 # and storing the numbers into the array
 for i in $(seq 10000); do
-
+    #
+    ### INSTRUCTOR COMMENT:
+    # Here 2>&1 redirects stderr into stdout. Why do you want error
+    # messages being saved inside your array?
+    #
     va=$(python fib.py $i 2>&1)
     array+=($va)
 done
 
+#
+### INSTRUCTOR COMMENT:
+# This is an extremely clever bash hack. Definitely include a
+# citation for where you get such a clever idea. Most coders probably
+# don't know such a hack if it is so clever, so documenting it carefully
+# is required.
+#
 # Combining all the values in our array, separated by a comma
 csv_combine=$(IFS=,; echo "${array[*]}")
 
@@ -27,11 +38,21 @@ csv_combine=$(IFS=,; echo "${array[*]}")
 file="./fibs.csv"
 back_file="./fibs.csv.bak"
 
+#
+### INSTRUCTOR COMMENT:
+# Logic like this should be at the beginning of the script. If your script is
+# going to abort itself, then you shouldn't have run all the computation
+# already.
+#
 # Checking to see if a back up file and if a copy of the file already exists
 # if so, will save accordingly, and if not, then will save a new file fibs.csv
 if [ -e "$back_file" ]
 then
     echo "Backup exists. Cannot overwrite, program will exit"
+    #
+    ###INSTRUCTOR COMMENT:
+    # Your forgot to exit 1
+    #
     :
 elif [ -e "$file" ]
 then
