4f08a091b7b9b44877739cb1a8248dfb4e1758aa
Justin Dressel <prof.justin.dressel@gmail.com>
2016-09-27 17:08:11 -0700

    instructor comments

--- test_sequences.py
+++ test_sequences.py
@@ -7,7 +7,13 @@ def test_fibonacci():
     fib_list=sequences.fibonacci(5)
     test_list=[1,1,2,3,5]
     assert fib_list == test_list
-
+    
+    #
+    ### INSTRUCTOR COMMENT:
+    # It is better to have each assert run in a separate test function. They are really separate tests that way.
+    # Also, it may be more clear in this case to skip defining so many intermediate variables:
+    #     assert sequences.fibonacci(1) == [1]
+    #
     fib_list=sequences.fibonacci(1)
     test_list=[1]
     assert fib_list == test_list
